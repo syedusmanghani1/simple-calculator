bb73f20a0ce861f9494b7326711fb9b75831d60e
Usman Ghani <syedusmanghani952@gmail.com>
2024-03-24 05:43:08 +0500

    push my calculator to github first time

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+// This line of code is called "Shabang"
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//conditional statements
+if (answer.operator === "Addition") {
+    console.log("Your value is: ");
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your value is: ");
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Your value is: ");
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log("Your value is: ");
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
